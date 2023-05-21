cb67b81b58914f2ee4a1624cfd57f10d554bfaa9
Jhonata Jackson <jhonata.monteiro@calindra.com.br>
2023-05-21 16:50:11 -0300

    feat: :sparkles: create VideoPlayer

---
+++ Africa/Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,20 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Jhonata Jackson on 21/05/23.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    
+    return videoPlayer!
+}
