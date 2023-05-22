3e577741bc41bf54f1cb9afee1c55b62a50b3ac5
Jhonata Jackson <jhonata.monteiro@calindra.com.br>
2023-05-22 13:04:55 -0300

    feat: :sparkles: add credits in contentview

---
+++ Africa/Africa/View/CreditsView.swift
@@ -0,0 +1,37 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Jhonata Jackson on 22/05/23.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        VStack {
+            Image("compass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 128, height: 128)
+            
+            Text("""
+            Copyright@ Jhonata Jackson
+            All right reserved
+            Better Apps 📱
+            """)
+            .font(.footnote)
+        .multilineTextAlignment(.center)
+        } //: VSTACK
+        .padding()
+        .opacity(0.4)
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
