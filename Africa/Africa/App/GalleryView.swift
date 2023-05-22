5d278943ee03e115d8d57c2b806f61819757f6cf
Jhonata Jackson <jhonata.monteiro@calindra.com.br>
2023-05-22 08:39:11 -0300

    feat: :sparkles: create gallery view with gridlayout and motion animation

---
+++ Africa/Africa/App/GalleryView.swift
@@ -0,0 +1,85 @@
+//
+//  GallaryView.swift
+//  Africa
+//
+//  Created by Jhonata Jackson on 20/05/23.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    // FUNCTIONS
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+            
+    // MARK: - BODY
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                // MARK - SLIDER
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        withAnimation(Animation.easeIn) {
+                            gridSwitch()
+                        }
+                        
+                    })
+                
+                // MARK: - GRID
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: LOOP
+                } //: GRID
+                .transition(.slide)
+                .onAppear {
+                    withAnimation(Animation.easeIn) {
+                        gridSwitch()
+                    }
+                }
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+        } //: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
+    }
+}
+
+// MARK: - PREVIEW
+
+struct GallaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
